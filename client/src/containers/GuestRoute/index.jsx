15afc8f750f9d4e464ca26aa6b41b21d0d7c133e
naz2231 <33528344+naz2231@users.noreply.github.com>
2020-05-19 21:02:09 +0300

    jsdoc

--- client/src/containers/GuestRoute/index.jsx
+++ client/src/containers/GuestRoute/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
+
 /**
  * Component for routing a guest
  * @param {object} props - component arguments
