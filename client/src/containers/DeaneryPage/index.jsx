b2265dd323363feecc673263ba077da8903e7c6b
naz2231 <33528344+naz2231@users.noreply.github.com>
2020-05-19 20:57:33 +0300

    jsdoc

--- client/src/containers/DeaneryPage/index.jsx
+++ client/src/containers/DeaneryPage/index.jsx
@@ -7,6 +7,9 @@ import SearchPanel from './tabs/SearchPanel';
 import SessionSchedule from './tabs/SessionSchedule';
 import AddSubject from './tabs/AddSubject'
 
+/**
+ * Array of current component's tabs for navigation defined in format approperiate for semantic-ui-react.Tab panes attribute
+ */
 const panes = [
   { menuItem: 'LessonsSchedule', render: () => <Tab.Pane><LessonsSchedule /></Tab.Pane> },
   { menuItem: 'SessionSchedule', render: () => <Tab.Pane><SessionSchedule /></Tab.Pane> },
@@ -15,6 +18,13 @@ const panes = [
   { menuItem: 'Add subject', render: () => <Tab.Pane><AddSubject /></Tab.Pane> },
 ]
 
+/**
+ * component for displaying menu tabs of Deanery
+ * 
+ * @returns (
+ *  <DeaneryPage />
+ * )
+ */
 const DeaneryPage = () => {
   return (
     <div>
