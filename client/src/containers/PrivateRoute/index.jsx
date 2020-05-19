6e3ecfcdccb7d7e40e915d4749efd26423e618cd
naz2231 <33528344+naz2231@users.noreply.github.com>
2020-05-19 21:05:41 +0300

    jsdoc

--- client/src/containers/PrivateRoute/index.jsx
+++ client/src/containers/PrivateRoute/index.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 
+/**
+ * Component for private routing
+ * 
+ * 
+ * @param {object} props - component arguments
+ * 
+ * @returns (
+ *  <PrivateRoute />
+ * )
+ */
 const PrivateRoute = (props) => {
   const { isAuthorized } = useSelector((state) => state.profile);
 
