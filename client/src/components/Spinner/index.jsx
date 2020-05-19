ea51ce3860aea2b26e9a7b858c212decb295d9dc
naz2231 <33528344+naz2231@users.noreply.github.com>
2020-05-19 18:56:34 +0300

    jsdoc

--- client/src/components/Spinner/index.jsx
+++ client/src/components/Spinner/index.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
-
+/**
+ * Component that displays when you make an async request to server
+ * 
+ * 
+ * @returns (
+ *  <Spinner />
+ * )
+ */
 const Spinner = () => {
   return (
     <div className="ui middle aligned center aligned grid">
