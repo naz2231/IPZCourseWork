e693424b12bd3f9eadc727ade1dc34898878a01d
naz2231 <33528344+naz2231@users.noreply.github.com>
2020-05-19 21:01:50 +0300

    jsdoc

--- client/src/containers/GuestRoute/index.jsx
+++ client/src/containers/GuestRoute/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
-
+/**
+ * Component for routing a guest
+ * @param {object} props - component arguments
+ * 
+ * @returns (
+ *  <GuestRoute user={user} />
+ * )
+ */
 const GuestRoute = props => {
   const { isAuthorized, user } = useSelector(state => state.profile);
 
